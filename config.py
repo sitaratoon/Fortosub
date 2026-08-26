be01cfcd657d3082b9a1843f7cbe6917a421bffc
sitaratoon <ajay393798@gmail.com>
2026-08-26 10:11:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "1234567"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-MONGO_DB = os.getenv("MONGO_DB", "YOUR_MONGO_URI")
+API_ID = int(os.getenv("API_ID", "18946488"))
+API_HASH = os.getenv("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7611196343:AAHRN3D-GsAZDROzcsiye7vT1HJI8AjkD5E")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://stoons:stoons@ajay.v5uug.mongodb.net/?retryWrites=true&w=majority")
 
 # Admin IDs comma separated string se parse hongi (e.g. "12345,67890")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()]
